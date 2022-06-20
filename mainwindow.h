bc6ebcbba7a242708d43477926a08070f307e96c
mingtiancai <2427373908@qq.com>
2022-06-20 19:26:32 +0800

    ENH: add some menu and set esc close main window

--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void keyPressEvent(QKeyEvent *event) Q_DECL_OVERRIDE;
 
 private:
     Ui::MainWindow *ui;
